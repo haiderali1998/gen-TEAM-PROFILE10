35594d56c7291812e66f513ccee8320d17e66648
haiderali9823-xSomaK <haiderali9823@outlook.com>
2022-09-06 17:52:54 +0100

    I am pushing this repo in order to seek help from ask bcs

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,20 @@
 
 const Engineer = require("../lib/Engineer")
-test("Engineer must be an object", () => {
-    
+
+test("Set up GitHub via a constructor", () => {
+    const testValue = "GithubUN";
+    const employee = new Engineer("John", 11, "test@john.co.uk", testValue);
+    expect(employee.github).toBe(testValue);
+})
+
+test("getRole()", () => {
+    const testValue = "Engineer";
+    const employee = new Engineer("John", 11, "test@john.co.uk", testValue);
+    expect(employee.getRole()).toBe(testValue);
+})
+
+test("Get gitHub", () => {
+    const testValue = "GithubUN";
+    const employee = new Engineer("John", 11, "test@john.co.uk", testValue);
+    expect(employee.getGitHub()).toBe(testValue);
 })
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern")
+
+test("Get school via constructor", () => {
+  const testValue = "University Of Birmingham";
+  const employee = new Intern("John", 11, "test@john.com", testValue);
+  expect(employee.school).toBe(testValue);
+});
+
+test("getRole()", () => {
+  const testValue = "Intern";
+  const employee = new Intern("John", 11, "test@john.com", "UCLA");
+  expect(employee.getRole()).toBe(testValue);
+});
+
+test("getSchool()", () => {
+  const testValue = "University Of Birmingham";
+  const employee = new Intern("John", 11, "test@john.com", testValue);
+  expect(employee.getSchool()).toBe(testValue);
+});
